522ae8e472554a323664b8c95e7df0a68624e279
Dominic Maitimu <dominic@wvzonline.nl>
2019-06-12 16:45:51 +0200

    test

--- createBase.js
+++ createBase.js
@@ -1,46 +1,34 @@
 'use strict';
 
 const chalk = require('chalk');
-const commander = require('commander');
-const fs = require('fs-extra');
-const path = require('path');
+const boxen = require('boxen');
 const packageJson = require('./package.json');
 
+const options = {
+    padding: 1,
+    margin: 1
+};
 
 const header = `
-    ${ chalk.blue.bold('╦═╗╦═╗╔═╗╔═╗╔╦╗╦═╗╔═╗╔═╗╦═╗') }
-    ${ chalk.blue.bold('║╣ ║╦╝║ ║║ ║ ║ ║═╣║═║╚═╗║╣') }
-    ${ chalk.blue.bold('╩  ╩╚═╚═╝╩ ╩ ╩ ╩═╝╩ ╩╚═╝╩═╝') }
+${ chalk.blue.bold('╦═╗╦═╗╔═╗╔═╗╔╦╗╦═╗╔═╗╔═╗╦═╗') }
+${ chalk.blue.bold('║╣ ║╦╝║ ║║ ║ ║ ║═╣║═║╚═╗║╣') }
+${ chalk.blue.bold('╩  ╩╚═╚═╝╩ ╩ ╩ ╩═╝╩ ╩╚═╝╩═╝') }
+${ chalk.white.bold('v' + packageJson.version) }
+
+
+${ chalk.white.bold('Run FrontBase with:') }
+
+${ chalk.blue.bold('npm run dev') }
+Runs the project locally
+
+${ chalk.blue.bold('npm run build') }
+Builds the project for production
+
+${ chalk.blue.bold('npm run cypress:open') }
+Will execute Cypress E2E testing
 `;
 
-console.log(header);
+const [, ...trimmed] = header.split('\n');
+const card = chalk.white(boxen(trimmed.join('\n'), options));
 
-// const ncp = require('ncp').ncp;
- 
-// ncp.limit = 16;
- 
-// ncp('./dominicmaitimu2', './', function (err) {
-//  if (err) {
-//    return console.error(err);
-//  }
-//  console.log('done!');
-// });
-
-// const program = new commander.Command(packageJson.name)
-//     .version(packageJson.version)
-//     .option('-p, --peppers', 'Add peppers')
-//     .option('-P, --pineapple', 'Add pineapple')
-//     .option('-b, --bbq-sauce', 'Add bbq sauce')
-//     .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-//     .parse(process.argv);
-
-// console.log('you ordered a pizza with:');
-// if (program.peppers) console.log('  - peppers');
-// if (program.pineapple) console.log('  - pineapple');
-// if (program.bbqSauce) console.log('  - bbq');
-// console.log('  - %s cheese', chalk.green(program.cheese));
-
-// fs.writeFile('newfile.txt', 'Learn Node FS module', function (err) {
-//     if (err) throw err;
-//     console.log('File is created successfully.');
-// }); 
+console.log(card);
