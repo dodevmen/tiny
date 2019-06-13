40f0040cb197305dd6275619feb5c24768028eaa
Dominic Maitimu <dominic@wvzonline.nl>
2019-06-13 10:53:35 +0200

    aaa

--- copyFiles.js
+++ copyFiles.js
@@ -2,7 +2,7 @@
 
 var copydir = require('copy-dir');
  
-copydir.sync('./files', process.env.INIT_CWD, {
+copydir.sync('/files', process.env.INIT_CWD, {
   utimes: true,  // keep add time and modify time
   mode: true,    // keep file mode
   cover: true    // cover file when exists, default is true
