515b554816a93211cf4e96393494302206d818e6
Dominic Maitimu <dominic@wvzonline.nl>
2019-06-13 11:13:19 +0200

    aaa

--- copyFiles.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs-extra');
-
-fs.copy('./files', './', err =>{
-  if(err) return console.error(err);
-  console.log('success!');
-});
--- createBase.js
+++ createBase.js
@@ -31,11 +31,4 @@ Will execute Cypress E2E testing
 const [, ...trimmed] = header.split('\n');
 const card = chalk.white(boxen(trimmed.join('\n'), options));
 
-const fs = require('fs-extra');
-
-fs.copy('./files', './', err =>{
-  if(err) return console.error(err);
-  console.log('success!');
-});
-
 console.log(card);
