0d7f1ec6eaffaca03be1392426418e52bedd3e80
Dominic Maitimu <dominic@wvzonline.nl>
2019-06-13 09:52:15 +0200

    hoi

--- createBase.js
+++ createBase.js
@@ -2,6 +2,7 @@
 
 const chalk = require('chalk');
 const boxen = require('boxen');
+const gentlyCopy = require('gently-copy');
 const packageJson = require('./package.json');
 
 const options = {
@@ -31,6 +32,9 @@ Will execute Cypress E2E testing
 const [, ...trimmed] = header.split('\n');
 const card = chalk.white(boxen(trimmed.join('\n'), options));
 
-console.log(card);
+const filesToCopy = ['*', 'files'];
+const userPath = process.env.INIT_CWD;
 
-require('./copyFiles');
+gentlyCopy(filesToCopy, userPath);
+
+console.log(card);
